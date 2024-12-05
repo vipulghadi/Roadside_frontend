1f5e02e402068332c2ff2ee3ae0e9ac2a7b6653a
vipulghadi <vipulvijayghadi@gmail.com>
2024-12-05 15:43:09 +0530

    updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import "./App.css";
 import { AuthProvider } from "./context/AuthProvider";
-import AppRoutes from "./Routes"; 
+import AppRoutes from "./routes";
 import NavbarClient from "./components/common/NavbarClient";
 
 function App() {
